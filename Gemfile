6a1760c7e4a6a0c3a0bbcb98237132d5e79dc795
Andre Pankratz <andre@webwarelab.com>
2010-09-02 16:33:01 +0200

    added RR

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "http://gemcutter.org"
 
 gem "rspec", "~> 2.0.0.beta.20"
+gem "rr"
 gem "relevance-rcov"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,9 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 
 require "rubygems"
 require "rspec"
+require "rr"
 require "vidibus-core_extensions"
+
+RSpec.configure do |config|  
+  config.mock_with :rr
+end

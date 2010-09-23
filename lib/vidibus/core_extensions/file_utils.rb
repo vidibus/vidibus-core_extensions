d12877e0966c9e6015eaa0bd743a0c2391a91a04
Andre Pankratz <andre@webwarelab.com>
2010-09-23 12:28:43 +0200

    Added FileUtils.remove_dir_r

---
+++ lib/vidibus/core_extensions/file_utils.rb
@@ -0,0 +1,24 @@
+module FileUtils
+
+  # Removes the current directory recursively including
+  # all empty parent directories up to given depth.
+  # The current directory will be removed with all its contents,
+  # but parent directories will only be removed if empty.
+  #
+  # Example:
+  #
+  #   FileUtils.remove_dir_r("/my/nice/private/dir", 2)
+  #   # will remove dir and, if empty, private as well
+  #
+  def self.remove_dir_r(dir, max_depth = 3)
+    FileUtils.remove_dir(dir)
+    parts = dir.split("/")
+    while max_depth > 1
+      parts.pop
+      max_depth -= 1
+      folder = File.join(parts)
+      break if (Dir.entries(folder) - ['.', '..']).any?
+      FileUtils.remove_dir(folder)
+    end
+  end
+end
---
+++ spec/vidibus/core_extensions/file_utils_spec.rb
@@ -0,0 +1,33 @@
+require "spec_helper"
+
+describe "Vidibus::CoreExtensions::FileUtils" do
+  describe ".remove_dir_r" do
+    let(:tmpdir) { File.join(File.dirname(__FILE__), "..", "..", "tmp") }
+    before { FileUtils.mkdir_p("#{tmpdir}/some/test/dir")}
+
+    it "should remove the current directory and all its parents up to three levels deep" do
+      FileUtils.remove_dir_r("#{tmpdir}/some/test/dir")
+      File.exist?("#{tmpdir}/some").should be_false
+      File.exist?(tmpdir).should be_true
+    end
+
+    it "should not remove parent directory unless it is emtpy" do
+      FileUtils.touch("#{tmpdir}/some/textfile.txt")
+      FileUtils.remove_dir_r("#{tmpdir}/some/test/dir")
+      File.exist?("#{tmpdir}/some").should be_true
+    end
+
+    it "should remove all child directories recursively" do
+      FileUtils.mkdir_p("#{tmpdir}/some/test/dir/with/some/children")
+      FileUtils.remove_dir_r("#{tmpdir}/some/test/dir")
+      File.exist?("#{tmpdir}/some").should be_false
+    end
+
+    it "should remove parent directories up to provided depth only" do
+      FileUtils.remove_dir_r("#{tmpdir}/some/test/dir", 2)
+      File.exist?("#{tmpdir}/some").should be_true
+    end
+
+    after { FileUtils.remove_dir(tmpdir)}
+  end
+end

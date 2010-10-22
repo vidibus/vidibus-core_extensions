27ba05094eb56be674de8b06886d8e53840fbee5
Andre Pankratz <andre@webwarelab.com>
2010-10-22 08:55:45 +0200

    Added an array method for merging a nested array and one for flattening with boundaries

--- lib/vidibus/core_extensions/array.rb
+++ lib/vidibus/core_extensions/array.rb
@@ -32,17 +32,73 @@ class Array
   #   [1, 'a'].merge([1, 2])      # => [1, 2, 'a']
   #   [1, 'a'].merge([3, 1, 2])   # => [3, 1, 2, 'a']
   #
-  def merge(array)
-    for value in array
-      next if include?(value)
-      if found = merging_predecessor(value, array)
+  def merge(array, options = {})
+    strict = options[:strict]
+    exclude = options[:exclude] || []
+    list = array.dup
+    for value in list
+      array.delete(value) and next if include?(value) or exclude.include?(value)
+      position = nil
+      if found = merging_predecessor(value, list)
         position = index(found) + 1
-      elsif found = merging_follower(value, array)
+      elsif found = merging_follower(value, list)
         position = index(found)
       end
-      insert(position || length, value)
+      if position or !strict
+        insert(position || length, value)
+        array.delete(value)
+      end
     end
     self
+  end
+
+  def merge_nested(array)
+    combined = self.dup
+    append = []
+    array.each_with_index do |a,i|
+      source = a.dup
+      combined.each_with_index do |c,j|
+        break if source.empty?
+        existing = combined.flatten_once - c
+        combined[j] = c.merge(source, :strict => true, :exclude => existing)
+      end
+      if source.any?
+        combined[i] ? combined[i].concat(source) : combined.last.concat(source)
+      end
+    end
+    combined
+  end
+
+  def flatten_with_boundaries(options = {})
+    prefix = options[:prefix] || "__a"
+    i = -1
+    inject([]) do |v,e|
+      if e.is_a?(Array)
+        i += 1
+        k = "#{prefix}#{i}"
+        v << k
+        v.concat(e)
+        v << k
+      else
+        v << e
+      end
+    end
+  end
+
+  def convert_boundaries(options = {})
+    prefix = options[:prefix] || "__a"
+    i = 0
+    boundaries = select {|a| a.match(/#{prefix}\d+/) if a.is_a?(String)}
+    return self unless boundaries.any?
+    array = self.dup
+    converted = []
+    for boundary in boundaries.uniq
+      converted.concat(array.slice!(0, array.index(boundary)))
+      array.shift
+      converted << array.slice!(0, array.index(boundary))
+      array.shift
+    end
+    converted.concat(array)
   end
 
   private
--- spec/vidibus/core_extensions/array_spec.rb
+++ spec/vidibus/core_extensions/array_spec.rb
@@ -55,5 +55,111 @@ describe "Vidibus::CoreExtensions::Array" do
     it "should merge [2,'b',1,'a'] with [5,3,6,7,1,2,4]" do
       [2,'b',1,'a'].merge([5,3,6,7,1,2,4]).should eql([2,4,'b',5,3,6,7,1,'a'])
     end
+
+    context "with :strict option" do
+      it "should merge [] with [1,2]" do
+        array = [1,2]
+        [].merge(array, :strict => true).should eql([])
+        array.should eql([1,2])
+      end
+
+      it "should merge [1,'a'] with [3,1,2]" do
+        array = [3,1,2]
+        [1,'a'].merge(array, :strict => true).should eql([3,1,2,'a'])
+        array.should be_empty
+      end
+    end
+  end
+
+  describe "#merge_nested" do
+    it "should merge [[]] with [[1,2]]" do
+      [[]].merge_nested([[1,2]]).should eql([[1,2]])
+    end
+
+    it "should merge [[]] with [[1],[2]]" do
+      [[]].merge_nested([[1],[2]]).should eql([[1,2]])
+    end
+
+    it "should merge [[],[]] with [[1],[2]]" do
+      [[],[]].merge_nested([[1],[2]]).should eql([[1],[2]])
+    end
+
+    it "should merge [[1],[]] with [[1],[2]]" do
+      [[1],[]].merge_nested([[1],[2]]).should eql([[1],[2]])
+    end
+
+    it "should merge [[1],[2]] with [[1],[2]]" do
+      [[1],[2]].merge_nested([[1],[2]]).should eql([[1],[2]])
+    end
+
+    it "should merge [[2],[1]] with [[1],[2]]" do
+      [[2],[1]].merge_nested([[1],[2]]).should eql([[2],[1]])
+    end
+
+    it "should merge [[2]] with [[1],[2]]" do
+      [[2]].merge_nested([[1],[2]]).should eql([[2,1]])
+    end
+
+    it "should merge [[2],[]] with [[1],[2]]" do
+      [[2],[]].merge_nested([[1],[2]]).should eql([[2,1],[]])
+    end
+    
+    it "should merge [[1,2,3]] with [[1,2],[3]]" do
+      [[1,2,3]].merge_nested([[1,2],[3]]).should eql([[1,2,3]])
+    end
+    
+    it "should merge [[1,2],[3]] with [[1],[2,3]]" do
+      [[1,2],[3]].merge_nested([[1],[2,3]]).should eql([[1,2],[3]])
+    end
+    
+    it "should keep source intact" do
+      source = [[1,2]]
+      [[1,2]].merge_nested(source)
+      source.should eql([[1,2]])
+    end
+  end
+
+  describe "#flatten_with_boundaries" do
+    it "should flatten [[1]]" do
+      [[1]].flatten_with_boundaries.should eql(["__a0",1,"__a0"])
+    end
+
+    it "should flatten [[1],2,[3]]" do
+      [[1],2,[3]].flatten_with_boundaries.should eql(["__a0",1,"__a0",2,"__a1",3,"__a1"])
+    end
+
+    it "should flatten [1,[2],3]" do
+      [1,[2],3].flatten_with_boundaries.should eql([1,"__a0",2,"__a0",3])
+    end
+
+    it 'should flatten [1,[2],3,4,[["x"]]]' do
+      [1,[2],3,4,[["x"]]].flatten_with_boundaries.should eql([1,"__a0",2,"__a0",3,4,"__a1",["x"],"__a1"])
+    end
+
+    it "should handle [1,2]" do
+      [1,2].flatten_with_boundaries.should eql([1,2])
+    end
+  end
+
+  describe "#convert_boundaries" do
+    it 'should convert ["__a0",1,"__a0"]' do
+      ["__a0",1,"__a0"].convert_boundaries.should eql([[1]])
+    end
+
+    it 'should convert ["__a0",1,"__a0",2,"__a1",3,"__a1"]' do
+      ["__a0",1,"__a0",2,"__a1",3,"__a1"].convert_boundaries.should eql([[1],2,[3]])
+    end
+
+    it 'should convert [1,"__a0",2,"__a0",3]' do
+      [1,"__a0",2,"__a0",3].convert_boundaries.should eql([1,[2],3])
+    end
+
+    it 'should convert [1,"__a0",2,"__a0",3,4,"__a1",["x"],"__a1"]' do
+      [1,"__a0",2,"__a0",3,4,"__a1",["x"],"__a1"].convert_boundaries.should eql([1,[2],3,4,[["x"]]])
+    end
+
+    it "should convert [1,2]" do
+      [1,2].convert_boundaries.should eql([1,2])
+    end
   end
 end

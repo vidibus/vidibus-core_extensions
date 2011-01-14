3f9ce0d120ea44ad9b14c816deb07880fcb38ceb
Andre Pankratz <andre@webwarelab.com>
2011-01-14 18:44:47 +0100

    Added Object#to_query method borrowed from core_ext

--- lib/vidibus/core_extensions/object.rb
+++ lib/vidibus/core_extensions/object.rb
@@ -13,4 +13,11 @@ class Object
       raise e
     end
   end
+
+  # Turns object into a key-value pair.
+  # Stolen from active_support/core_ext
+  def to_query(key)
+    require "cgi" unless defined?(CGI) && defined?(CGI::escape)
+    "#{CGI.escape(key.to_s)}=#{CGI.escape(to_s)}"
+  end
 end

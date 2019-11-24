ae7e87e2f630c2443290174d77e8fadf508f13e1
André Pankratz <andre@vidibus.com>
2019-11-24 20:01:01 +0100

    Remove Object#try!
    This method is provided by Rails now.

--- lib/vidibus/core_extensions/object.rb
+++ lib/vidibus/core_extensions/object.rb
@@ -1,19 +1,4 @@
 class Object
-
-  # Tries to use a method on object.
-  #
-  # Example:
-  #   something.try!(:else) # => nil
-  #
-  def try!(method)
-    begin
-      send(method)
-    rescue NoMethodError
-    rescue => e
-      raise e
-    end
-  end
-
   # Turns object into a key-value pair.
   # Stolen from active_support/core_ext
   def to_query(key)
